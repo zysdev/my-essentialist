42ffd9f26dbef9647a1a4d134c43556b13cae2e1
zys <ajismaulana.id@gmail.com>
2024-08-17 14:29:05 +0700

    Initial Commit

---
+++ src/persona.js
@@ -0,0 +1,52 @@
+export const persona = {
+  firstName: "Zys",
+  secondName: "Dev",
+  description: "Celestial Codecraft: A Space Enthusiast Programmer.",
+  email: "zysdevv@gmail.com",
+  story:
+    "Where the lines of a programmer's code intertwine with the cosmic tapestry, and the heart of an astrophile beats in harmony with the rhythms of the universe.",
+  sections: {
+    // You can create your own sections. But! For reducing headache in future, change the values (content), but don't change the keys ('title', 'desc').
+    techs: [
+      {
+        title: "Astro",
+        desc: "Main Framework",
+        url: "https://astro.build",
+      },
+      {
+        title: "Tailwind CSS",
+        desc: "CSS Framework",
+        url: "https://tailwindcss.com",
+      },
+      {
+        title: "React JS",
+        desc: "UI Framework",
+        url: "https://reactjs.com",
+      },
+    ],
+    experience: [
+      {
+        title: "Raihan Media",
+        desc: "2021-now",
+        url: "#",
+      },
+      {
+        title: "Diconic Studio",
+        desc: "2018-2020",
+        url: "#",
+      },
+    ],
+  },
+  contacts: [
+    {
+      title: "x",
+      url: "https://twitter.com/ajismaulana_id",
+    },
+    {
+      title: "ig",
+      url: "https://instagram.com/ajismaulana.id",
+    },
+  ],
+};
+
+// Don't forget to save upon changing the contents of the file ;)
